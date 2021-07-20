1508ce38b20d0cbc942348a7a7a323b3614d3ae6
Wagner Martins <martinswagner021@gmail.com>
2021-07-20 00:27:34 -0300

    Get all, create and delete card methods done, modify is still not avaible.

--- src/routes/card.js
+++ src/routes/card.js
@@ -1,9 +1,11 @@
 // Libraries Imports
 const express = require('express')
 const router = express.Router()
+const URL = require('url')
 
 // Models Imports
 const Card = require('../models/card')
+
 
 // Router
 
@@ -11,33 +13,52 @@ const Card = require('../models/card')
 router.get('/', async (req, res) => {
     const { user_id } = req
 
+    // Get all the cards by user_id
     const cards = await Card.find({ creator: user_id })
     
-    res.send({ cards })
+    return res.send({ cards })
 })
+
 // Create cards
 router.post('/', async (req, res) => {
     const { user_id } = req
     const { title, content } = req.body
 
+    // Check if the fields are correctly filled up
     if(!content) {
         return res.send({error: "Your card does not have any content."})
     }
 
+    // Create the card
     const card = await Card.create({ title: title, content: content, creator: user_id })
 
-    res.send({ card })
+    return res.send({ card })
 })
 
 // Modify cards
 router.put('/', (req, res) => {
     const { user_id } = req
-    res.send({error: "hello",user:user_id})
+    const { title, content } = req.body
+    
+    // Check if the fields are correctly filled up
+    if(!content) {
+        return res.send({error: "Your card does not have any content."})
+    }
+
+    
+
+    return res.send({error: "in production..."})
 })
 
 // Delete cards
-router.delete('/', (req, res) => {
-
+router.delete('/', async (req, res) => {
+    
+    // Get the id from the element to be deleted through URL query
+    const { id } = URL.parse(req.url, true).query
+    
+    await Card.deleteOne({_id: id})
+    
+    return res.send({message: "Card successfully deleted."})
 })
 
 // Exports
