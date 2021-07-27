387ec519c0d45fe5a2ba40e615c760a59e56a1d7
Wagner Martins <martinswagner021@gmail.com>
2021-07-27 18:59:12 -0300

    Authenticator's error messages update

--- src/middlewares/authenticator.js
+++ src/middlewares/authenticator.js
@@ -8,13 +8,13 @@ const Authenticator = (req, res, next) => {
     const { authorization } = req.headers
 
     // Check if there is any token provided 
-    if(!authorization) return res.status(401).send({message: "You need to log in at first!"})
+    if(!authorization) return res.status(401).send({error: "You need to log in at first!"})
     
     // Split the token 
     const [ Bearer , token ] = authorization.split(' ', 2)
 
     // Check if the token is well formed
-    if(!Bearer || !token) return res.status(401).send({message: "Malformed token!"})
+    if(!Bearer || !token) return res.status(401).send({error: "Malformed token!"})
 
     // Check if the token is still actually valid
     try {
