1022feba5292bd9ac0717d15f947ac946ba8d0ff
Wagner Martins <martinswagner021@gmail.com>
2021-08-03 19:19:45 -0300

    Heroku deploy updates

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,5 @@
 // Libraries Imports
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 // Connection to the database
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
